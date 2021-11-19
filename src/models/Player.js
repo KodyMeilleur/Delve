751f7f37ed11d0b53a9e52d79fc34a4b82747db7
unknown <kodymeilleur@yahoo.com>
2021-11-19 18:35:58 -0500

    alucard example

--- src/models/Player.js
+++ src/models/Player.js
@@ -11,7 +11,7 @@ export function DefaultPlayer(name, x, y) {
   this.movingVerticalOffset = 0;
   this.movingHorizontalOffset = 0;
   this.sprite = 'assets/Characters/Human/';
-  this.animation = new Animation(1, 'Idle', true);
+  this.animation = new Animation(4, 'Idle', true);
 
   this.name = name;
   this.isCreature = true;
