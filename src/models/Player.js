ecd1f697e1e5540114851fb5f3ae192a09db5596
unknown <kodymeilleur@yahoo.com>
2022-01-11 16:04:34 -0500

    added stone

--- src/models/Player.js
+++ src/models/Player.js
@@ -3,6 +3,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { Animation } from './Animation.js';
 import { MagicBean } from '../models/items/nurture/MagicBean.js';
 import { Wood } from '../models/items/resources/Wood.js';
+import { Stone } from '../models/items/resources/Stone.js';
 
 export function DefaultPlayer(name, x, y) {
 
@@ -39,7 +40,7 @@ export function DefaultPlayer(name, x, y) {
   this.pts = CONST.startingStatPoints;
 
   this.technologies = [];
-  this.items = [new MagicBean(3), new Wood(1)];
+  this.items = [new MagicBean(3), new Wood(1), new Stone(5)];
 
   this.equipment = {
     leftHand: {},
--- src/models/items/resources/Stone.js
+++ src/models/items/resources/Stone.js
@@ -1,4 +1,10 @@
 export function Stone (quantity) {
-  this.sprite = '';
+  this.sprite = 'assets/Icons/Items/Resources/commonStone.png';
   this.quantity = quantity;
+  this.name = "Common Stone";
+  this.description = 'A slab of stone from the flatlands. Many a kingdom have risen and fallen, all built on this rock.';
+
+  this.effect = () => {
+
+  }
 }
