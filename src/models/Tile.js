69b314934b7374b36214c835d602704bf249bf13
unknown <kodymeilleur@yahoo.com>
2022-01-14 00:25:06 -0500

    variant updates

--- src/models/Tile.js
+++ src/models/Tile.js
@@ -44,8 +44,8 @@ export function PlainsTile(x, y) {
   this.density = 0;
   this.event = null;
   // this.event = (this.structure === null && (getRandomInt(0, 3) === 2)) ? new Event('Alert') : null;
-  this.variants = 6;
-  this.variant = (getRandomInt(0, this.variants - 1) < 2) ? getRandomInt(0, this.variants - 1) : null;
+  this.variants = 8;
+  this.variant = (getRandomInt(0, this.variants - 1) < 3) ? getRandomInt(0, this.variants - 1) : null;
   this.sprite = `assets/Tiles/Plains/Platform/sheet${this.variant !== null ? this.variant : ''}.png`;
   this.potentialPath = false;
   this.type = 'Plains';
