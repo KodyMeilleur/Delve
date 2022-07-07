e514622f29e33ad35f835ee207a249f7a1366009
unknown <kodymeilleur@yahoo.com>
2022-07-07 18:33:46 -0400

    added eyeling hurt animation

--- src/components/Monster.vue
+++ src/components/Monster.vue
@@ -238,7 +238,7 @@ export default {
     },
     handleSkillEffect({monsterID, damage}) {
       if (monsterID === this.monster.id && this.monster.isDead === false) {
-        this.animation = new Animation(3, 'hurt', false);
+        this.animation = new Animation(2, 'hurt', false);
         this.lastDamageSuffered = damage;
         this.monster.hp = (this.monster.hp - damage < 0) ? 0 : this.monster.hp - damage;
         this.shrinkGrow = true;
