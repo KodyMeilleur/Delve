037850de60bc23c1f0bec0fab5b0c4e751f1454f
unknown <kodymeilleur@yahoo.com>
2022-08-01 02:01:06 -0400

    added spinner sprites

--- src/models/monsters/Eyeling.js
+++ src/models/monsters/Eyeling.js
@@ -1,25 +1,6 @@
 import { Monster } from '../Monster';
 import { Animation } from '../Animation.js';
 
-// export function Dummy(x, y) {
-//   Monster.call(this, x, y);
-//
-//   this.movement = 0; // moving points
-//   this.maxMovement = 0; // moving points
-//   this.type = 'Dummy';
-//   this.name = 'Dummy';
-//   this.sprite = 'assets/Characters/Dummy/';
-//   this.animation = new Animation(1, 'Idle', true);
-//   this.defaultAnimation = this.animation;
-//
-//   // STATS
-//   this.hp = 20;
-//   this.might = 0;
-//   this.toughness = 0;
-//   this.intelligence = 0;
-//   this.arcana = 0;
-// }
-
 export function Eyeling(x, y, isBattling) {
   Monster.call(this, x, y);
 
